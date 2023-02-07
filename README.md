043ca4f8f5da0be88d0a731463d1817c4202598c
PradeepSiddh <114388943+PradeepSiddh@users.noreply.github.com>
2023-02-07 20:09:51 +0530

    Update and rename README.md to README.rs

--- README.md
+++
@@ -1,1 +0,0 @@
-# Concordium-Environment-setup
---
+++ README.rs
@@ -0,0 +1,13 @@
+---
+Title: "Environment Setup of Concordium"
+Description: "How to Setup development environment."
+Keywords: 
+   1. Blockchain
+   2. Web3
+   3. Rust
+   4. Concordium
+   5. DApp
+   
+----
+
+
